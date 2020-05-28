43a2e762e812eb3cbac2849871d43bbc5adea6d9
Jy <alf.batman@mail.ru>
2020-05-28 15:19:49 -0400

    Add laravel project

---
+++ blog/resources/views/inc/hero.blade.php
@@ -0,0 +1,8 @@
+<div class="jumbotron">
+  <div class="container">
+    <h1>Привет всем на нашем сайте!</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+      exercitation.</p>
+  </div>
+</div>
